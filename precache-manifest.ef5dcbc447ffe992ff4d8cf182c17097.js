a47ffac34bb0158e18da78a58d8af78954d24468
vaniaparamita <vania.paramita23@gmail.com>
2019-03-28 08:38:32 +0700

    Updates

---
+++ precache-manifest.ef5dcbc447ffe992ff4d8cf182c17097.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52508b6a1c80208a722f",
+    "url": "/reactjs2/static/js/runtime~main.876720e8.js"
+  },
+  {
+    "revision": "3685a8889b1ed9b551a7",
+    "url": "/reactjs2/static/js/main.374bdc50.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/reactjs2/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "3685a8889b1ed9b551a7",
+    "url": "/reactjs2/static/css/main.f1266f54.chunk.css"
+  },
+  {
+    "revision": "98be4c1dcc7404585543ad7466135852",
+    "url": "/reactjs2/index.html"
+  }
+];
